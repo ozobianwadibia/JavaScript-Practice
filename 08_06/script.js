cfa28c633c589077ac360493e6adfa12bfa9c3df
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2021-06-21 09:56:09 -0500

    Completed problem 08_06

--- 08_06/script.js
+++ 08_06/script.js
@@ -5,3 +5,29 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+const articleObject = {
+  mainHeading: "New article heading",
+  subHeading: "A long long time ago, in a far far away galaxy...",
+};
+
+const articleContent = `<header> 
+<h1>${articleObject.mainHeading}</h1>
+<p>${articleObject.subHeading}</p>
+</header>`;
+
+function makeArticle() {
+  const article = document.createElement("article");
+  const main = document.querySelector("main");
+  article.innerHTML = articleContent;
+  main.appendChild(article);
+}
+
+const changeHeading = function () {
+  const heading = document.querySelector("h1");
+  heading.innerText = "This is the new main Heading";
+  heading.style.color = "red";
+};
+
+makeArticle();
+
+setTimeout(changeHeading, 5000);
